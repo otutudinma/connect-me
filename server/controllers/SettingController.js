2b09f4469a76f3045ed68fb0b83cde6ece508176
otutudinma <cotutu95@gmail.com>
2019-04-08 18:30:30 +0100

    users can add bank accounts

--- server/controllers/SettingController.js
+++ server/controllers/SettingController.js
@@ -18,5 +18,66 @@ const TYPE_NAME = 'users';
  * @class SettingController
  */
 class SettingController {
-    
+    /**
+   *@description Creates a new wallet for a new user
+   *@static
+   *@param  {Object} req - request
+   *@param  {Object} res - request
+   *@returns {object} - null
+   *@memberof SettingController
+   */
+  static async addBankData(req, res) {
+    try {
+      const {
+        phoneNumber,
+        bankName,
+        accountNumber,
+        beneficiaryName
+      } = req.body;
+      if (!phoneNumber || !bankName || !accountNumber || !beneficiaryName) {
+        return res.status(400).json(
+          responses.error(400, 'Account already added, cannot be done twice')
+        );
+      }
+      const retrievedUser = await SettingController.retrieveUser(res, phoneNumber);
+      if (!retrievedUser.phoneNumber) return;
+
+      const {
+        banks
+      } = retrievedUser;
+
+      const accountExist = banks && banks.find(bank => bank.accountNumber === accountNumber);
+      if (accountExist) {
+        return res.status(400).json(
+          responses.error(400, 'Account already added, cannot be done twice')
+        );
+      }
+      const updatedProfile = await User.findOneAndUpdate({
+        phoneNumber
+      }, {
+        $addToSet: {
+          banks: {
+            bankName,
+            accountNumber,
+            beneficiaryName
+          }
+        }
+      }, {
+        new: true
+      });
+
+      const handledQueue = SettingController.handleQueue(updatedProfile);
+      if (handledQueue) {
+        return res.status(200).json(
+          responses.success(200, 'Bank data successfully added', updatedProfile.banks)
+        );
+      }
+    } catch (error) {
+      traceLogger(error);
+      return res.status(500).json(
+        responses.error(500, 'Server error, failed to add bank details')
+      );
+    }
+  }
+
 }
