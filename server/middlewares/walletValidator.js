22feec684f5e6fcda55b3431ea78d6da3f9bbf44
otutudinma <cotutu95@gmail.com>
2019-04-09 08:03:38 +0100

    codeVerification validator

--- server/middlewares/walletValidator.js
+++ server/middlewares/walletValidator.js
@@ -66,6 +66,21 @@ const walletValidator = {
 
     validationErrorHandler(req, res, next);
   },
+
+  codeVerificationValidator: (req, res, next) => {
+    req.check('phoneNumber', 'phoneNumber should be 14 digits').isLength({
+      min: 14,
+      max: 14
+    });
+    req.check('phoneNumber', 'phoneNumber is required and should be a number e.g +234xxxxxxxxxx').isInt();
+    req.check('passCode', 'passCode should be 4 digits').isLength({
+      min: 4,
+      max: 4
+    });
+    req.check('passCode', 'passCode is required and should be a number e.g xxxx').isInt();
+
+    validationErrorHandler(req, res, next);
+  },
 };
 
 export default walletValidator;
