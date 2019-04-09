d8b8c8e4fef2a19fa66bb1b6649cae762c17c0b8
otutudinma <cotutu95@gmail.com>
2019-04-09 07:47:51 +0100

    create wallet for users

--- server/controllers/WalletController.js
+++ server/controllers/WalletController.js
@@ -12,3 +12,51 @@ import ConnectionController from './ConnectionController';
 import SettingController from './SettingController';
 import config from '../config';
 import notification from '../utils/notification';
+
+const INDEX_NAME = 'halaapp';
+const TYPE_NAME = 'wallet';
+
+const {
+    retrieveWallet,
+  } = SettingController;
+
+  /**
+ * @description Defines the actions to for the wallet endpoints
+ * @class WalletController
+ */
+class WalletController {
+    /**
+   *@description Creates a new wallet for a new user
+   *@static
+   *@param  {Object} userPhoneNumber - request
+   *@returns {object} - null
+   *@memberof walletController
+   */
+  static async newWallet(userPhoneNumber) {
+    const newWallet = await Wallet.create({
+      phoneNumber: userPhoneNumber
+    });
+    if (!newWallet) throw new Error();
+    const {
+      phoneNumber,
+      isActivated,
+      passCode,
+      totalAmount,
+      securityQuestion,
+      securityAnswer
+    } = newWallet;
+    const walletObject = {
+      phoneNumber,
+      isActivated,
+      passCode,
+      totalAmount,
+      securityQuestion,
+      securityAnswer,
+      transactionReference: [],
+      merchantReference: [],
+      cardTokens: []
+    };
+    elastic.addData(`${INDEX_NAME}-${TYPE_NAME}`, TYPE_NAME, phoneNumber, walletObject, esResponse => esResponse);
+  }
+
+}
